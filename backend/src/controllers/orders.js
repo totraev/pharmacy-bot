e9fa69db9c81e3f077dd25e522886b16cc7d7549
David Totraev <totraev@gmail.com>
2017-01-15 13:47:18 +0300

    add orders ctrl

--- backend/src/controllers/orders.js
+++ backend/src/controllers/orders.js
@@ -1,9 +1,25 @@
+import Order from '../models/orders'
+
 export default {
 	index(req, res) {
-		res.send('not implemented')
+		Order.find({}, (err, orders) => {
+			if(err) {
+				res.status(500).send()
+			} else {
+				res.status(200).json(orders)
+			}
+		})
 	},
 
 	view(req, res) {
-		res.send('not implemented')
+		const { id } = req.params
+
+		Order.findById(id, (err, order) => {
+			if(err) {
+				res.status(500).send()
+			} else {
+				res.status(200).json(order)
+			}
+		})
 	}
-} 
+}
