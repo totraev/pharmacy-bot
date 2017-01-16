83a14bda2ecd99a45cb961a4bf474874d0fc10a3
David Totraev <totraev@gmail.com>
2017-01-17 00:19:44 +0300

    fix create product

--- frontend/src/app/pages/product.create/index.js
+++ frontend/src/app/pages/product.create/index.js
@@ -3,11 +3,11 @@ import angular from 'angular'
 import ProductCreateCtrl from './controller'
 import tpl from './product.pug'
 
-export default angular.module('page.product', [])
+export default angular.module('page.productcreate', [])
   .controller('ProductCreateCtrl', ProductCreateCtrl)
   .config(($stateProvider) => {
     $stateProvider.state('admin.productcreate', {
-      url: '/products/create',
+      url: '/product/create',
       template: tpl,
       controller: 'ProductCreateCtrl',
       controllerAs: 'vm'
